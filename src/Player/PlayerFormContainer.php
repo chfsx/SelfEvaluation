06f72d19688abdd22343c013381ce66ac45f7108
Fabian Schmid <fabian@sr.solutions>
2024-07-11 08:16:59 +0200

    [FIX] wrong property declaration

--- src/Player/PlayerFormContainer.php
+++ src/Player/PlayerFormContainer.php
@@ -25,11 +25,7 @@ class PlayerFormContainer extends ilPropertyFormGUI
      * @var int
      */
     protected $question_field_size = 6;
-
-    /**
-     * @var ilGlobalTemplateInterface
-     */
-    protected $global_tpl;
+    
 
     /**
      * @var ilRepositoryObjectPlugin
